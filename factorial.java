be8e9b769a36bf670f81c22f6dacc3d837e24a53
Nishithayatam <91303119+Nishithayatam@users.noreply.github.com>
2021-12-02 10:59:02 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PEC
+ */
+public class factorial { 
+    public static void main(String args[])
+    {
+        int i,fact=1;  
+        int number=5;//It is the number to calculate factorial    
+        for(i=1;i<=number;i++)
+        {
+            fact=fact*i;
+        }    
+        System.out.println("Factorial of "+number+" is: "+fact);    
+    }  
+}  
+    
+
